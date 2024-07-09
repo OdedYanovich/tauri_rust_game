2870c951ae79607257f6b3959c84589bb8cdbbea
Oded Yanovich <s1o2s3o4s5o6@gmail.com>
2024-07-09 14:49:38 +0300

    Make new command with availableRanges

--- src/scripts/levels.js
+++ src/scripts/levels.js
@@ -9,9 +9,12 @@ export const commandBi = 0;
 const commandNbi = 1;
 export const levels = [
   new Level(2, 1, [[commandBi]]),
-  new Level(2, 1, [[commandNbi]]),
+  new Level(2, 1, [[commandNbi]]), //2
   new Level(2, 1, [[commandBi, commandNbi]]),
-  new Level(3, 1, [[commandBi]]),
+  new Level(3, 1, [[commandBi]]), //4
   new Level(3, 1, [[commandNbi]]),
-  new Level(3, 1, [[commandBi, commandNbi]]),
+  new Level(3, 1, [[commandBi, commandNbi]]), //6
+  new Level(3, 2, [[commandBi]]),
+  new Level(3, 2, [[commandNbi]]), //8
+  new Level(3, 2, [[commandBi,commandNbi]]),
 ];
--- src/scripts/mods/fight.js
+++ src/scripts/mods/fight.js
@@ -2,36 +2,41 @@ import { levels, commandBi } from "../levels.js";
 import { levelsID } from "../dom.js";
 import { chosenLevel } from "./menu.js";
 
-const fightContent = document.querySelector("#GameContent");
-const levelButtonsMax = ["f", "g", "h", "j", "k"];
-let levelsButtons;
-var potentialSequences;
-let commandRows;
-let currentLevel; //Hopefully the main loop will not need a direct access to the level
-
-const healthHTML = document.querySelector(".health");
-let progressLost = 0;
-let currentPotentialSequences;
-
+const progressLostMax = 50;
+let progressLost = progressLostMax;
 setInterval(() => {
-  if (progressLost < 50) progressLost += 0.02;
+  if (progressLost < progressLostMax) progressLost += 0.02;
   healthHTML.style.setProperty("--progressLost", progressLost + "%");
 }, 1);
 
-progressLost = 50;
+const fightContent = document.querySelector("#GameContent");
+const levelButtonsMax = ["f", "g", "h", "j", "k"];
+let levelsButtons;
+var ranges;
+let commandRows;
+let currentLevel;
+
+const healthHTML = document.querySelector(".health");
+let currentPotentialSequences;
+let incompleteSequence;
+
 const newCommand = () => {
-  currentPotentialSequences = structuredClone(potentialSequences);
-  const selectedButtonIndex = Math.floor(
-    Math.random() * currentPotentialSequences[0].length
+  currentPotentialSequences = structuredClone(ranges);
+  let availableRanges = ranges.keys();console.log(availableRanges);
+  const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+  const selectedCommandIndex = getRandomIndex(currentLevel.commands[0]);
+  const selectedRangeIndex = getRandomIndex(currentPotentialSequences);
+  const selectedButtonIndex = getRandomIndex(
+    currentPotentialSequences[selectedRangeIndex]
   );
-  const selectedCommandIndex = Math.floor(
-    Math.random() * currentLevel.commands[0].length
-  );
-  commandRows[0].innerText = currentPotentialSequences[0][selectedButtonIndex];
+
+  commandRows[0].innerText =
+    currentPotentialSequences[selectedRangeIndex][selectedButtonIndex] +
+    (selectedRangeIndex + 1);
   if (currentLevel.commands[0][selectedCommandIndex] === commandBi) {
-    currentPotentialSequences[0] = currentPotentialSequences[0].filter(
-      (e, i) => i === selectedButtonIndex
-    );
+    currentPotentialSequences[selectedRangeIndex] = currentPotentialSequences[
+      selectedRangeIndex
+    ].filter((e, i) => i === selectedButtonIndex);
     commandRows[0].style.border = "none";
   } else {
     commandRows[0].style.border = "solid";
@@ -40,13 +45,10 @@ const newCommand = () => {
 };
 export const fightInit = () => {
   currentLevel = levels[chosenLevel - 1];
-  levelsButtons = [];
-  for (let i = 0; i < currentLevel.buttons; i++)
-    levelsButtons.push(levelButtonsMax[i]);
-
-  potentialSequences = [];
-  for (let i = 0; i < currentLevel.presses; i++)
-    potentialSequences.push(levelsButtons);
+  progressLost = progressLostMax;
+  incompleteSequence = [];
+  levelsButtons = levelButtonsMax.slice(0, currentLevel.buttons);
+  ranges = Array(currentLevel.presses).fill(levelsButtons);
 
   commandRows = [];
   fightContent.innerHTML = "";
@@ -54,31 +56,30 @@ export const fightInit = () => {
     commandRows.push(document.createElement("d"));
     fightContent.appendChild(commandRows[i]);
   }
-  // healthHTML.style.setProperty("--progressLost", progressLost + "%");
   newCommand();
 };
-
 export const fightFn = (key) => {
   if (levelsButtons.includes(key)) {
-    if (
-      currentPotentialSequences[0].find((element) => element === key) !==
-      undefined
-    ) {
+    incompleteSequence.push(key);
+    if (incompleteSequence.length !== currentLevel.presses) return;
+    let result = true;
+    for (let index = 0; index < currentLevel.presses; index++) {
+      if (
+        currentPotentialSequences[index].find(
+          (element) => element === incompleteSequence[index]
+        ) === undefined
+      ) {
+        result = false;
+        break;
+      }
+    }
+    if (result) {
       progressLost -= 8;
       if (progressLost < 0) return levelsID;
-    } else progressLost += 8;
+    } else if (progressLost < progressLostMax) progressLost += 8;
+    else progressLost = progressLostMax;
+    incompleteSequence = [];
     newCommand();
   }
-  // let turnsAvailableButtons = levelsButtons;
-  // let turnPotentialSequenceIndex = []; // If the potential presses only has 1 button then his index is removed from the array
-  // for (let i = 0; i < turnPotentialSequences.length; i++)
-  //   turnPotentialSequenceIndex.push(i);
-  // currentLevel.commands.array.forEach((element) => {
-  //   let turnsPotentialPresses =
-  //     turnPotentialSequences[
-  //       turnPotentialSequenceIndex[
-  //         Math.floor(Math.random() * turnAmbiguousSequenceIndex.length)
-  //       ]
-  //     ];
-  // });
 };
+window.addEventListener("keyup", () => (incompleteSequence = []));
